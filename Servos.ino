cfa8296599a045196e07e1a939a02a5f519a7e9d
rocioqo <rocioqo87@gmail.com>
2016-07-20 13:49:33 -0600

    Add files via upload

---
+++ Servos.ino
@@ -0,0 +1,50 @@
+#include <Servo.h>
+
+Servo myservo;
+Servo myservoY;
+int posX, posY;
+
+void setup()
+{
+Serial.begin(9600);
+myservo.attach(9);
+myservoY.attach(10);
+  
+}
+
+void loop()
+{
+ int val=map(posX,0,600,0,180);
+  myservo.write(val);
+  int val2=map(posY,0,600,0,180);
+  myservoY.write(val2);
+  
+ // delay(50);
+  }
+                                                                            
+void serialEvent() {
+  if (Serial.available())
+  {
+    posX = Serial.parseInt();
+    posY = Serial.parseInt();
+
+  }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
