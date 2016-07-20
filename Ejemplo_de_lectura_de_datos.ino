ed23d27519401954dd16856b943167c7bcf2a2e3
rocioqo <rocioqo87@gmail.com>
2016-07-20 13:48:07 -0600

    Add files via upload

---
+++ Ejemplo_de_lectura_de_datos.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode (A0,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+// para concatenar el string que va a leer el Processing
+Serial.print(analogRead(A0));
+Serial.print(",");
+Serial.println(analogRead(A0));
+delay(500);
+// Herramientas serial porter
+}
